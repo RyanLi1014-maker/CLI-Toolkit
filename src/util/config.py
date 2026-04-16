a3beba86da2b38ea9fc82d824e7ea9ac7684152c
RyanLi1014-marker <ryanli1014@outlook.com>
2026-04-17 05:08:47 +0800

    fix: correct typo in config_file_path variable name

--- src/util/config.py
+++ src/util/config.py
@@ -20,28 +20,28 @@ class Config(dict):
         """
         super().__init__()  # Initialize the dictionary
         self.logger = logging.getLogger("Config")
-        self.confg_file_path = Path("config") / config_path
+        self.config_file_path = Path("config") / config_path
         # Check if the config directory exists
-        if not self.confg_file_path.parent.exists():
+        if not self.config_file_path.parent.exists():
             self.logger.warning("Config directory does not exist. Creating...")
-            self.confg_file_path.parent.mkdir(parents=True)
+            self.config_file_path.parent.mkdir(parents=True)
         self.default = default_config
 
     def load(self):
         """Load the configuration from the file."""
-        if self.confg_file_path.exists():  # Check if the configuration file exists
-            with open(self.confg_file_path, "r") as f:
+        if self.config_file_path.exists():  # Check if the configuration file exists
+            with open(self.config_file_path, "r") as f:
                 try:
                     content = json.load(f)
                 except json.JSONDecodeError:
                     self.logger.warning(
-                        f"File '{self.confg_file_path.name}' is not a valid json file. Returning default configuration."
+                        f"File '{self.config_file_path.name}' is not a valid json file. Returning default configuration."
                     )
                     content = self.default
                 self.update(content)  # Load the configuration from the file
         else:  # If the configuration file does not exist, return the default configuration
             self.logger.warning(
-                f"File '{self.confg_file_path.name}' does not exist. Returning default configuration and saving it to the file."
+                f"File '{self.config_file_path.name}' does not exist. Returning default configuration and saving it to the file."
             )
             self.update(
                 self.default
@@ -51,10 +51,10 @@ class Config(dict):
     def save(self):
         """Save the configuration to the file."""
         # Check if the configuration directory exists
-        if not self.confg_file_path.parent.exists():
+        if not self.config_file_path.parent.exists():
             self.logger.warning("Config directory does not exist. Creating...")
-            self.confg_file_path.parent.mkdir(parents=True)
-        with open(self.confg_file_path, "w") as f:
+            self.config_file_path.parent.mkdir(parents=True)
+        with open(self.config_file_path, "w") as f:
             json.dump(self, f, indent=4)
 
     def __getitem__(self, key):
